4028606b59b3d39204b3e1737919e0a67296922d
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2019-08-21 11:49:15 +0800

    add file system maker

--- debian/debian_init.py
+++ debian/debian_init.py
@@ -74,6 +74,7 @@ def install_embedded_tools():
     os.system("apt install i2c-tools spi-tools can-utils -y")
     os.system("apt install gdb gdbserver gdb-multiarch -y")
     os.system("apt install gcc-arm-linux-gnueabi gcc-arm-none-eabi gcc-arm-linux-gnueabihf -y")
+    os.system("apt install mtd-utils -y")
 
 def install_wireshark(user):
     os.system("apt install wireshark -y")
