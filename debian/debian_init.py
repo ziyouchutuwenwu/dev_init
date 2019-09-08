e1ae43245fd7a3984e54e60943cdda8be549dd2a
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2019-09-08 16:17:42 +0800

    squashfs

--- debian/debian_init.py
+++ debian/debian_init.py
@@ -50,7 +50,7 @@ def install_embedded_tools():
     os.system("apt install i2c-tools spi-tools can-utils -y")
     os.system("apt install gdb gdbserver gdb-multiarch -y")
     os.system("apt install gcc-arm-linux-gnueabi gcc-arm-none-eabi gcc-arm-linux-gnueabihf -y")
-    os.system("apt install mtd-utils -y")
+    os.system("apt install mtd-utils squashfs-tools -y")
 
 def install_wireshark(user):
     os.system("apt install wireshark -y")
