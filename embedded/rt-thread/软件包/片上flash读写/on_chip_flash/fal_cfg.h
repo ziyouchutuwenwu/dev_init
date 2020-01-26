9cd494da5c7080ad562d139899c2f8f591f92d83
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-01-26 10:01:02 +0800

    rtt的日常更新

--- embedded/rt-thread/软件包/片上flash读写/on_chip_flash/fal_cfg.h
+++ embedded/rt-thread/软件包/片上flash读写/on_chip_flash/fal_cfg.h
@@ -1,36 +1,12 @@
-/*
- * File      : fal_cfg.h
- * This file is part of FAL (Flash Abstraction Layer) package
- * COPYRIGHT (C) 2006 - 2018, RT-Thread Development Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Change Logs:
- * Date           Author       Notes
- * 2018-05-17     armink       the first version
- */
-
 #ifndef _FAL_CFG_H_
 #define _FAL_CFG_H_
 
 #include <rtconfig.h>
 #include <board.h>
 
-#define STM32_FLASH_START_ADRESS_16K 0x08000000
-#define STM32_FLASH_START_ADRESS_64K (0x08000000 + (512-64*3)*1024)
-#define STM32_FLASH_START_ADRESS_128K 0x08000000
+#define STM32_FLASH_START_ADRESS_16K STM32_FLASH_START_ADRESS
+#define STM32_FLASH_START_ADRESS_64K (STM32_FLASH_END_ADDRESS - 64*3*1024)
+#define STM32_FLASH_START_ADRESS_128K STM32_FLASH_START_ADRESS
 
 #define FLASH_SIZE_GRANULARITY_16K 16*1024
 #define FLASH_SIZE_GRANULARITY_64K 64*3*1024
