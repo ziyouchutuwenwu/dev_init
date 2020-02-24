b29782b2ea9a3607a1a6ab550e7364df52c73950
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-02-24 17:03:45 +0800

    更新electron配置

--- web/electron/electron_config.sh
+++ web/electron/electron_config.sh
@@ -2,9 +2,9 @@
 
 npm config set registry "https://registry.npm.taobao.org"
 npm config set electron_mirror "https://npm.taobao.org/mirrors/electron/"
+npm config set chromedriver_cdnurl "https://npm.taobao.org/mirrors/chromedriver"
+npm config set sass_binary_site "https://npm.taobao.org/mirrors/node-sass/"
 
-# npm config set disturl https://npm.taobao.org/dist
-
-echo "electron7以后，淘宝镜像比官方在url里面少了一个v，需要配置这个"
+echo "electron7以后，淘宝镜像比官方在url里面少了一个v，需要配置"
 # 处理官方和淘宝镜像站的区别
 npm config set electron_custom_dir 8.0.1
