4b04ace0c2b60a6b354be2f5f70fa611509b8ebf
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-03-02 13:21:31 +0800

    添加binwalk

--- python/pip_init.py
+++ python/pip_init.py
@@ -9,39 +9,42 @@ from py_mods import file
 
 if __name__ == "__main__":
 
-    cmd="pip install --upgrade pip"
+    cmd = "pip install --upgrade pip"
     os.system(cmd)
 
-    cmd="pip install sqlmap"
+    cmd = "pip install stegoveritas-binwalk"
     os.system(cmd)
 
-    cmd="pip install pysocks"
+    cmd = "pip install sqlmap"
     os.system(cmd)
 
-    cmd="pip install pyqt5 PyQtWebEngine"
+    cmd = "pip install pysocks"
     os.system(cmd)
 
-    cmd="pip install requsts"
+    cmd = "pip install pyqt5 PyQtWebEngine"
     os.system(cmd)
 
-    cmd="pip install mycli"
+    cmd = "pip install requsts"
     os.system(cmd)
 
-    cmd="pip install pgcli"
+    cmd = "pip install mycli"
     os.system(cmd)
 
-    cmd="pip install httpie"
+    cmd = "pip install pgcli"
     os.system(cmd)
 
-    cmd="pip install pip-review"
+    cmd = "pip install httpie"
     os.system(cmd)
 
-    cmd="pip install you-get"
+    cmd = "pip install pip-review"
     os.system(cmd)
 
-    cmd="pip install youtube-dl"
+    cmd = "pip install you-get"
     os.system(cmd)
 
-    cmd="pip install pyinotify"
+    cmd = "pip install youtube-dl"
+    os.system(cmd)
+
+    cmd = "pip install pyinotify"
     os.system(cmd)
     file.set_to_profile("alias pyfilemon=\"python -m pyinotify -v\"")
