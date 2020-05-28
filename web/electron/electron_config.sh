fa0211fc203956ee4c2e088cdba176c07d7fa48a
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-05-28 17:33:42 +0800

    chromedriver换镜像

--- web/electron/electron_config.sh
+++ web/electron/electron_config.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env /bin/bash
 
 npm config set registry "https://registry.npm.taobao.org"
+npm config set chromedriver_cdnurl "http://cdn.npm.taobao.org/dist/chromedriver"
 npm config set electron_mirror "https://npm.taobao.org/mirrors/electron/"
 npm config set sass_binary_site "https://npm.taobao.org/mirrors/node-sass/"
 
