76ff91956ffaa3b6499be8c10450740244c3650d
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-05-28 17:38:23 +0800

    更新

---
+++ nvm/npm_config.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env /bin/bash
+
+npm config set registry "https://registry.npm.taobao.org"
+npm config set chromedriver_cdnurl "http://cdn.npm.taobao.org/dist/chromedriver"
+npm config set sass_binary_site "https://npm.taobao.org/mirrors/node-sass/"
--- web/electron/electron_config.sh
+++ web/electron/electron_config.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env /bin/bash
 
-npm config set registry "https://registry.npm.taobao.org"
-npm config set chromedriver_cdnurl "http://cdn.npm.taobao.org/dist/chromedriver"
 npm config set electron_mirror "https://npm.taobao.org/mirrors/electron/"
-npm config set sass_binary_site "https://npm.taobao.org/mirrors/node-sass/"
 
 echo "electron7以后，淘宝镜像比官方在url里面少了一个v，需要配置"
 # 处理官方和淘宝镜像站的区别
