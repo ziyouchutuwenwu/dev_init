9d81b85f9fc8bec02e59f3d56c085d9fcb8b8be4
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-06-10 17:10:18 +0800

    rb脚本更新

--- ruby/rb-set-mirror.py
+++ ruby/rb-set-mirror.py
@@ -3,6 +3,20 @@
 
 import os
 
+
+def is_cmd_in_path(cmd):
+    is_cmd_exist = False
+    for cmdpath in os.environ["PATH"].split(":"):
+        if os.path.isdir(cmdpath) and cmd in os.listdir(cmdpath):
+            is_cmd_exist = True
+            break
+    return is_cmd_exist
+
+
+if not is_cmd_in_path("gem"):
+    print("please install ruby first")
+    exit(-1)
+
 os.system(
     "gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/"
 )
