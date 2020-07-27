1ea4f14b3fb5cfa105704e273378f47ddefe92ce
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-07-27 19:55:40 +0800

    openocd脚本更新

--- embedded/openocd/unprotect.py
+++ embedded/openocd/unprotect.py
@@ -5,14 +5,13 @@ import os
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("stm32 board not found, f1, f4, h7 ?")
+        print("usage: %s board_type xxx.bin" % sys.argv[0])
         exit(-1)
 
     board_type = sys.argv[1]
-    board_full_name = "stm32%sx" % board_type
 
     cmd = (
         "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c init -c 'reset halt' -c '%s unlock 0' -c 'reset halt' -c shutdown"
-        % (board_full_name, board_full_name)
+        % (board_type, board_type)
     )
     os.system(cmd)
