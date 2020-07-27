2d163453f428b14d94f4aff7ed0ccd5135c642b7
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-07-27 16:40:56 +0800

    更新脚本

--- embedded/openocd/burn.py
+++ embedded/openocd/burn.py
@@ -4,15 +4,17 @@ import sys
 import os
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("stm32 board not found, f1, f4, h7 ?")
+    if len(sys.argv) != 3:
+        print("usage: %s board_type xxx.bin" % sys.argv[0])
+        print("board_type can be f1 f4 h7")
         exit(-1)
 
     board_type = sys.argv[1]
+    bin_name = sys.argv[2]
     board_full_name = "stm32%sx" % board_type
 
     cmd = (
-        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c init -c 'reset halt' -c 'flash write_image erase rtthread.bin 0x08000000' -c 'verify_image rtthread.bin 0x08000000' -c 'reset run' -c shutdown"
-        % board_full_name
+        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c init -c 'reset halt' -c 'flash write_image erase %s 0x08000000' -c 'verify_image %s 0x08000000' -c 'reset run' -c shutdown"
+        % (board_full_name, bin_name, bin_name)
     )
     os.system(cmd)
