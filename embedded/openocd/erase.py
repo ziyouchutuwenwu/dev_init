f053e17183237d1ac028c7613728014516bd2a04
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-07-29 20:22:48 +0800

    更新rtt

--- embedded/openocd/erase.py
+++ embedded/openocd/erase.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     board_type = sys.argv[1]
 
     cmd = (
-        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c init -c 'reset halt' -c 'flash info 0' -c 'flash erase_sector 0 0 last' -c 'flash erase_check 0' -c shutdown"
+        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c 'init' -c ' reset_config none' -c 'reset halt' -c 'flash info 0' -c 'flash erase_sector 0 0 last' -c 'flash erase_check 0' -c shutdown"
         % board_type
     )
     os.system(cmd)
--- embedded/openocd/unprotect.py
+++ embedded/openocd/unprotect.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     board_type = sys.argv[1]
 
     cmd = (
-        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c init -c 'reset halt' -c '%s unlock 0' -c 'reset halt' -c shutdown"
+        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c 'init' -c 'reset halt' -c '%s unlock 0' -c 'reset halt' -c shutdown"
         % (board_type, board_type)
     )
     os.system(cmd)
