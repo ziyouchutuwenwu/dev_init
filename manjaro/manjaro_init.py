e6e18b6245ab72300b691573cdf00df1b20ea284
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-11-11 10:07:25 +0800

    添加manjaro

---
+++ manjaro/manjaro_init.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+
+import os
+import sys
+
+sys.path.append("..")
+from py_mods import proc
+
+
+def do_set_mirror_config():
+    os.system(
+        "pacman-mirrors -i -c China -m rank"
+    )
+
+
+def do_upgrade():
+    os.system(
+        "pacman -Syyu --noconfirm"
+    )
+
+def install_essential_fonts():
+    os.system(
+        "pacman -S --noconfirm ttf-monaco"
+    )
+    os.system(
+        "pacman -S --noconfirm ttf-roboto noto-fonts ttf-dejavu"
+    )
+    os.system(
+        "pacman -S --noconfirm wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei"
+    )
+    os.system(
+        "pacman -S --noconfirm noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts"
+    )
+
+def install_chinese_input():
+    os.system(
+        "pacman -S --noconfirm fcitx-sunpinyin fcitx-im fcitx-configtool"
+    )
+    os.system(
+        "cp ./xprofile_template ~/.xprofile"
+    )
+
+def install_themes(user):
+    proc.run_as_user(user, "mkdir -p ~/.themes")
+    proc.run_as_user(user, "cp -rf ./themes/* ~/.themes")
+
+def do_zprezto_config(user):
+    os.system("pacman -S --noconfirm zsh")
+    proc.run_as_user(user, "sh ./zprezto/config.sh")
+
+def install_pyenv(user):
+    os.system("pacman -S --noconfirm pyenv")
+    proc.run_as_user(user, "mkdir -p ~/.pip")
+    proc.run_as_user(user, "cp ./python/pip.conf ~/.pip/")
+
+def install_essential_tools():
+    os.system("pacman -S --noconfirm git")
+
+if __name__ == "__main__":
+
+    if False == proc.is_root():
+        print("This program must be run as root. Aborting.")
+        exit(-1)
+
+    login_user = os.getlogin()
+
+    do_set_mirror_config()
+    do_upgrade()
+
+    install_essential_fonts()
+    install_chinese_input()
+    install_pyenv()
+    install_themes(login_user)
+
+    # 太卡了，放最后
+    do_zprezto_config(login_user)
---
+++ manjaro/zprezto/config.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env /bin/bash
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+ln -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zlogin" ~/.zlogin
+ln -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zlogout" ~/.zlogout
+ln -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zpreztorc" ~/.zpreztorc
+ln -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile" ~/.zprofile
+ln -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshenv" ~/.zshenv
+ln -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshrc" ~/.zshrc
+
+sed -i "s/theme 'sorin'/theme 'skwp'/g" ${ZDOTDIR:-$HOME}/.zprezto/runcoms/zpreztorc
+
+echo "sudo pacman -Syyu" > ~/.zhistory
+chmod -w ~/.zhistory
+
+rm -rf ~/.bashrc
+rm -rf ~/.bash_history
+rm -rf ~/.bash_logout
+
+# ------------------------------------------------------------------------
+# 抄袭fish的语法高亮的主题
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting
+echo "source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+# 增加fish的补全插件
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+echo "source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+echo "if repo clone failed, run \"cd ~/.zprezto; git submodule update --recursive\""
+
+mkdir -p ~/.zsh/colors
+CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
+cp -rf $CURRENT_DIR/shell_color/dircolors.ansi-dark ~/.zsh/colors
+
+echo "eval \`dircolors ~/.zsh/colors/dircolors.ansi-dark\`\n" >> ~/.zshrc
