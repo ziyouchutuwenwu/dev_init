170a2860294592338a935f483463097149537487
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-11-21 14:47:18 +0800

    更新jlink

--- embedded/openocd/burn.py
+++ embedded/openocd/burn.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     bin_name = sys.argv[2]
 
     cmd = (
-        "openocd -f interface/jlink.cfg -c 'transport select swd' -f target/%s.cfg -c 'init' -c ' reset_config none' -c 'reset halt' -c 'flash write_image erase %s 0x08000000' -c 'verify_image %s 0x08000000' -c 'reset run' -c shutdown"
+        "openocd -f interface/jlink.cfg -d2 -c 'transport select swd' -f target/%s.cfg -c 'init' -c ' reset_config none' -c 'reset halt' -c 'flash write_image erase %s 0x08000000' -c 'verify_image %s 0x08000000' -c 'reset run' -c shutdown"
         % (board_type, bin_name, bin_name)
     )
     os.system(cmd)
