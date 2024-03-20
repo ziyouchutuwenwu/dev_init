69e681ed39740042420c7fec92c65d2f88864ee1
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2024-03-20 14:41:47 +0800

    .

--- development/programing/beam/elixir/爬虫/spider/deploy/clean.sh
+++ development/programing/beam/elixir/爬虫/spider/deploy/clean.sh
@@ -5,10 +5,10 @@ docker kill xxx-spider-redis
 docker kill xxx-spider-splash
 docker kill xxx-spider-tika
 
-docker rm xxx-spider
-docker rm xxx-spider-redis
-docker rm xxx-spider-splash
-docker rm xxx-spider-tika
+docker rm -v xxx-spider
+docker rm -v xxx-spider-redis
+docker rm -v xxx-spider-splash
+docker rm -v xxx-spider-tika
 
 docker network rm xxx-spider-network
 docker rmi xxx-spider:latest --force
