1ab99fd10f4ead2448a461755effbe64c8287a36
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2024-11-16 21:42:11 +0800

    .

--- os/freebsd/zsh/config.sh
+++ os/freebsd/zsh/config.sh
@@ -12,9 +12,9 @@ cp -rf $CURRENT_DIR/zinit/zshrc ~/.zshrc
 chmod -w ~/.zshrc
 
 echo "sudo pkg update -fq; sudo pkg upgrade -y; sudo pkg autoremove -y; sudo pkg clean -ay" > ~/.zsh_history
-echo "sudo freebsd-update fetch install" >> ~/.zsh_history
 echo "asdf update" >> ~/.zsh_history
 echo "zinit self-update; zinit delete --clean -y; zinit update" >> ~/.zsh_history
+echo "sudo freebsd-update fetch install" >> ~/.zsh_history
 chmod -w ~/.zsh_history
 
 rm -rf ~/.bashrc
