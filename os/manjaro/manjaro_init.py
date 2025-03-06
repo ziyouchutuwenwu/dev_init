9f0883ab2e543e246db317f41d76b9d08a14b10c
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-03-06 18:51:27 +0800

    .

--- os/manjaro/manjaro_init.py
+++ os/manjaro/manjaro_init.py
@@ -312,7 +312,6 @@ def install_ansible_essential():
 
 def install_ssh_server():
     os.system("yes | pacman --noconfirm -S openssh")
-    os.system("sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config")
     os.system("sed -i 's/^#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config")
     os.system("systemctl restart sshd; systemctl enable sshd")
 
