787e928bbaf0fa137ff2a4fe79d9f83df9241118
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-03-23 14:21:54 +0800

    .

--- development/editor/nvim/install.sh
+++ development/editor/nvim/install.sh
@@ -4,6 +4,7 @@ CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
 
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
 
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 cp -rf $CURRENT_DIR/lua/*.lua ~/.config/nvim/lua/plugins/
