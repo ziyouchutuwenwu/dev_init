4a93e6ab97ffd01b469be61b4794e9b8db32b3db
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-03-27 15:42:46 +0800

    .

--- development/ai/ocr/ctllers.py
+++ development/ai/ocr/ctllers.py
@@ -1,5 +1,7 @@
+import asyncio
 import ocr
 from robyn import jsonify
+
 
 # curl --location --request POST 'http://127.0.0.1:8080/ocr' \
 # --form 'image=@"/home/mmc/desktop/111.png"'
@@ -12,7 +14,8 @@ async def on_ocr(request):
 
         file_name = next(iter(request.files))
         img_bin = request.files[file_name]
-        result = ocr.do_ocr(img_bin)
+        # result = ocr.do_ocr(img_bin)
+        result = await asyncio.to_thread(ocr.do_ocr, img_bin)
         return jsonify(result)
 
     except Exception as e:
