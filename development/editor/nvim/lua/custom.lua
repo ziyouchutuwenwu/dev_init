171a76bde7d0cf00107a2415ff5a4f4d3939e030
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-05-06 16:55:01 +0800

    .

--- development/editor/nvim/lua/custom.lua
+++ development/editor/nvim/lua/custom.lua
@@ -2,16 +2,8 @@ return {
   {
     "nvimtools/none-ls.nvim",
     opts = function(_, _)
-      -- 设置换行
+      -- 自动换行
       vim.cmd('set wrap')
-    end,
-  },
-  {
-    "AstroNvim/AstroNvim",
-    opts = function(_, _)
-      -- 上下键代替 C-N 和 C-P
-      vim.cmd('cnoremap <Up> <C-p>')
-      vim.cmd('cnoremap <Down> <C-n>')
     end,
   }
 }
