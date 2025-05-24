e5d8eb879d658ee5ac5bffc58b572bb5db974949
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-05-25 07:25:23 +0800

    .

--- os/freebsd/zsh/global/zsh/keymap.zsh
+++ os/freebsd/zsh/global/zsh/keymap.zsh
@@ -2,8 +2,8 @@
 
 bindkey "\e[1~" beginning-of-line
 bindkey "\e[4~" end-of-line
-bindkey "\e[5~" beginning-of-history
-bindkey "\e[6~" end-of-history
+bindkey "^[[5~" up-line-or-history
+bindkey "^[[6~" down-line-or-history
 bindkey "\e[3~" delete-char
 bindkey "\e[2~" quoted-insert
 bindkey "\e[5C" forward-word
