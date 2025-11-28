e553ba7b258447988f199eee5dfdedac890deacf
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-11-28 10:00:49 +0800

    .

--- os/freebsd/profile.d/ls.sh
+++ os/freebsd/profile.d/ls.sh
@@ -1,3 +1,2 @@
-alias ll='ls -la -G' 2>/dev/null
-alias l.='ls -G -d .*' 2>/dev/null
 alias ls='ls -G' 2>/dev/null
+alias ll='ls -l -G' 2>/dev/null
--- os/voidlinux/profile/profile.d/ls.sh
+++ os/voidlinux/profile/profile.d/ls.sh
@@ -1,3 +1,2 @@
-alias ll='ls -la --color=auto' 2>/dev/null
-alias l.='ls --color=auto -d .*' 2>/dev/null
 alias ls='ls --color=auto' 2>/dev/null
+alias ll='ls -l --color=auto' 2>/dev/null
