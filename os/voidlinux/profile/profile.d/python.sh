2ed1b6cea4e60ffa9a41aeebafb87ea4f5546b21
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-12-15 14:30:26 +0800

    .

--- os/voidlinux/profile/profile.d/python.sh
+++ os/voidlinux/profile/profile.d/python.sh
@@ -1,4 +1,7 @@
 export PYTHONPYCACHEPREFIX=/dev/null
 
+# 允许多个 index
+export UV_INDEX_STRATEGY=unsafe-best-match
+
 export UV_PYTHON_INSTALL_MIRROR="https://registry.npmmirror.com/-/binary/python-build-standalone/"
 export UV_INDEX="https://mirrors.aliyun.com/pypi/simple"
