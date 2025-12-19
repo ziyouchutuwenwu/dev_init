38319f716bd431a84066215b9bcecca73a122294
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-12-19 14:54:27 +0800

    .

--- development/editor/nvim/user/plugins/custom.lua
+++ development/editor/nvim/user/plugins/custom.lua
@@ -1,9 +1,4 @@
-return {
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, _)
-      -- 自动换行
-      vim.cmd('set wrap')
-    end,
-  }
-}
+-- 自动换行
+vim.cmd('set wrap')
+
+return {}
--- development/editor/nvim/user/plugins/osc52.lua
+++ development/editor/nvim/user/plugins/osc52.lua
@@ -1,19 +1,14 @@
-return {
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, _)
-      -- 剪贴板支持
-      vim.g.clipboard = {
-        name = "osc 52",
-        copy = {
-          ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-          ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-        },
-        paste = {
-          ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-          ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-        },
-      }
-    end,
-  }
+-- 剪贴板支持
+vim.g.clipboard = {
+  name = "osc 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
 }
+
+return {}
