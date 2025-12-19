4301feefc6efffa95fe0a6786a2069328d607a5c
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-12-19 19:35:57 +0800

    .

--- development/editor/nvim/user/plugins/osc52.lua
+++ development/editor/nvim/user/plugins/osc52.lua
@@ -1,13 +1,21 @@
-vim.g.clipboard = {
-  name = "osc 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+local is_ssh = vim.env.SSH_CONNECTION ~= nil or vim.env.SSH_TTY ~= nil
+
+local has_gui_clipboard = vim.env.DISPLAY ~= nil or 
+                         vim.env.WAYLAND_DISPLAY ~= nil or 
+                         vim.env.XDG_SESSION_TYPE == "wayland"
+
+if is_ssh and not has_gui_clipboard then
+  vim.g.clipboard = {
+    name = "osc 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
 
 return {}
