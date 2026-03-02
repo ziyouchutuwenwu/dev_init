d13e51117fe40b50a7384f233021f4e6f277a266
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2026-03-02 17:18:36 +0800

    .

--- development/editor/nvim/install.sh
+++ development/editor/nvim/install.sh
@@ -9,7 +9,7 @@ rm -rf ~/.local/state/nvim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 
 mkdir -p ~/.config/nvim/lua/
-cp -rf $CURRENT_DIR/*.lua ~/.config/nvim/lua/plugins/
-cp -rf $CURRENT_DIR/user ~/.config/nvim/lua/
+cp -rf $CURRENT_DIR/lua/*.lua ~/.config/nvim/lua/plugins/
+cp -rf $CURRENT_DIR/lua/user ~/.config/nvim/lua/
 
 echo "Install success!";
