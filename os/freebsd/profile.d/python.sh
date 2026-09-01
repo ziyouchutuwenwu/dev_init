e6db1f90051e1871506686c813ef1be3b0f6535b
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2026-09-01 09:16:07 +0800

    .

--- os/freebsd/profile.d/python.sh
+++ os/freebsd/profile.d/python.sh
@@ -1,2 +1,1 @@
 export PYTHONPYCACHEPREFIX=/dev/null
-export UV_PYTHON_INSTALL_MIRROR="https://registry.npmmirror.com/-/binary/python-build-standalone/"
--- os/voidlinux/profile/profile.d/uv.sh
+++ os/voidlinux/profile/profile.d/uv.sh
@@ -1,5 +1,9 @@
-# 优先级最低
-export UV_DEFAULT_INDEX="https://mirrors.aliyun.com/pypi/simple"
+# uv python install 3.11
+export UV_PYTHON_INSTALL_MIRROR="https://registry.npmmirror.com/-/binary/python-build-standalone/"
 
+# 三方包的源
 # 从第一个开始匹配
 # export UV_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cu124 https://download.pytorch.org/whl/cu132"
+
+# 优先级最低
+export UV_DEFAULT_INDEX="https://mirrors.aliyun.com/pypi/simple"
